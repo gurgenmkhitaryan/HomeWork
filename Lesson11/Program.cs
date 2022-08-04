9e360c30880022af42b3b54871358b741cfa360d
rico <gugomkh@mail.ru>
2022-08-04 20:54:55 +0300

    Lesson 12

---
+++ Lesson11/Program.cs
@@ -0,0 +1,23 @@
+﻿
+// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+// 3, 5 -> 243 (3⁵)
+
+// 2, 4 -> 16
+
+while(true)
+{
+    Console.Write("Введите первое число: ");
+
+    int ferstDigit = Convert.ToInt32(Console.ReadLine());
+    
+    Console.Write("Введите второе число: ");
+
+    int secondDigit = Convert.ToInt32(Console.ReadLine());
+
+    Console.WriteLine(Math.Pow(ferstDigit, secondDigit));
+
+}
+
+
+
---
+++ Lesson12/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+// 452 -> 11
+
+// 82 -> 10
+
+// 9012 -> 12
+
+// Console.Write("Введите число: ");
+
+// int number = Convert.ToInt32(Console.ReadLine());
+
+// int digit = 0;
+// int sum = 0;
+// while (number != 0)
+// {
+//     number /= 10;
+//     sum = sum + digit;
+// }
+
+// Console.WriteLine($"Сумма чисел: {digit}");
+
+Console.Write("Введите число: ");
+
+int number = Convert.ToInt32(Console.ReadLine());
+int countPositive = 0;
+while (number != 0)
+{
+number /= 10;
+countPositive++; 
+
+}
+
+Console.WriteLine($"Количество чисел: {countPositive}");
